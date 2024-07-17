f29fa9cb4b80334851638164de90501ef8d13676
tr-eren.ugurlu <erentrugurlu@gmail.com>
2024-07-17 17:55:00 +0300

    initial commit
    Müşteri Arşivleme Sistemi

---
+++ src/test/java/com/customer/arc/CustomerArchivingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.arc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerArchivingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
